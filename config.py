0e2d33ca22910876194773f0380649aff776d9ef
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-09-11 15:41:45 -0400

    adding env var compatibility for config

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 import json
+from pathlib import Path
+from os import getenv
 
-with open('config.json') as json_data_file:
+json_data_file = Path('config.json')
+# checking if config exists
+if json_data_file.resolve().exists():
     data = json.load(json_data_file)
+
+# else load environment variables
+else:
+    env_vars = [ 'ENDPOINT', 'CERTPATH', 'APIKEY' ]
+    data = {}
+    for env_var in env_vars:
+        data[env_var.lower()] = getenv(env_vars)
